6c329800d87a60cb0382cfa57114057d201aa242
IceLily636 <24196ks@hvhs.school.nz>
2026-04-29 03:00:06 +0000

    Apr 29, 2026, 3:00 AM

--- t03_maths.js
+++ t03_maths.js
@@ -17,9 +17,9 @@ let birthyear ="2011"
 let oldage;
 let newmoney;
 let newermoney;
-newmoney= money/2;
-oldage= age + 10;
-newermoney=newmoney+3;
+newmoney = money/2;
+oldage = age + 10;
+newermoney = newmoney+3;
 /****************************
  main code
  **********************/
@@ -30,7 +30,7 @@ newermoney=newmoney+3;
  ***************************/
 console.log("Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars");
 console.log("You were born in " + birthyear )
-console.log("In 10 years you will be " +oldage+ " years old")
+console.log("In 10 years you will be " + oldage + " years old")
 console.log("You have " +money+ " dollars")
 console.log("You spend half of your money, now you have" + newmoney+ "dollars")
 console.log("Then you get $3, now you have " +newermoney+ " dollars")
