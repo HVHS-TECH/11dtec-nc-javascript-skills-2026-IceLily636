f411b382e1fb5a38c91633cfec6273e5a3bc7c82
IceLily636 <24196ks@hvhs.school.nz>
2026-04-29 02:34:14 +0000

    Apr 29, 2026, 2:34 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,24 @@
-/* Paste your code from task 2 here */
+let username = "Kayla"
+let year = "2026"
+let age ="15"
+let money ="200"
+
+console.log("Hi" + username +"As of " + year + "you are " + age + "years old. As of " + year + "you have " + money );
+ 
+/****************************
+Name of task: variables
+****************************/
+console.log("t02_variables.js")
+console.log("hi")
+
+// variables
+
+/****************************
+ main code
+ **********************/
+
+ 
+ /*****************************
+ functions
+ ***************************/
+
