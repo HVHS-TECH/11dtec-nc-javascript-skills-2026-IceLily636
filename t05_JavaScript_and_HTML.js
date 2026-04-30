0fc245856141d196384e62fdcfe05a2240cb387c
IceLily636 <24196ks@hvhs.school.nz>
2026-04-30 21:37:51 +0000

    Apr 30, 2026, 9:37 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,38 @@
 /* Paste your code from task 3, Maths here */
+
+
+
+ 
+/****************************
+Name of task: maths
+****************************/
+console.log("t02_variables.js")
+console.log("hi")
+
+// variables
+let username = "Kayla"
+let year = 2026
+let age =15
+let money =200
+let birthyear =2011
+let oldage;
+let newmoney;
+let newermoney;
+newmoney = money/2;
+oldage = age + 10;
+newermoney = newmoney+3;
+/****************************
+ main code
+ **********************/
+
+
+ 
+ /*****************************
+ functions
+ ***************************/
+console.log("Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars");
+console.log("You were born in " + birthyear )
+console.log("In 10 years you will be " + oldage + " years old")
+console.log("You have " +money+ " dollars")
+console.log("You spend half of your money, now you have" + newmoney+ "dollars")
+console.log("Then you get $3, now you have " +newermoney+ " dollars")
