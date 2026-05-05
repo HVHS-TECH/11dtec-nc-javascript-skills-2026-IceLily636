a49bc443bc534fcd9649801768afb6185a337b58
IceLily636 <24196ks@hvhs.school.nz>
2026-05-05 00:11:17 +0000

    May 5, 2026, 12:11 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -29,11 +29,14 @@ console.log("In 10 years you will be " + oldage + " years old")
 console.log("You have " +money+ " dollars")
 console.log("You spend half of your money, now you have" + newmoney+ "dollars")
 console.log("Then you get $3, now you have " +newermoney+ " dollars")
-item1("Chocolate bar",4)
+
 /****************************
  main code
  **********************/
 writeline();
+item1("Chocolate bar ",4)
+item1("Chips ",3)
+item1("Drink ",2.50)
 OUTPUT.innerHTML+="<h2>Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars</h2>";
 OUTPUT.innerHTML+="<h2> You were born in " + birthyear+ "</h2>";
 OUTPUT.innerHTML+="<h2>In 10 years you will be " + oldage + " years old</h2>";
