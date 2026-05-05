84a6c3a164936972010cd3a284fe717b248550d5
IceLily636 <24196ks@hvhs.school.nz>
2026-05-05 00:02:54 +0000

    May 5, 2026, 12:02 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -20,8 +20,8 @@ oldage = age + 10;
 newermoney = newmoney+3;
 function writeline(){
     OUTPUT.innerHTML = "<h1>welcome to the shop</h1>";}
-function item1(_name){
-OUTPUT
+function item1(_name, _price){
+OUTPUT.innerHTML +=
 }
 console.log("Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars");
 console.log("You were born in " + birthyear)
