7166a04990ced5f7a0c89c5d952c265149ad105c
IceLily636 <24196ks@hvhs.school.nz>
2026-05-07 21:44:39 +0000

    May 7, 2026, 9:44 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -37,7 +37,7 @@ console.log("Then you get $3, now you have " +newermoney+ " dollars")
 /****************************
  main code
  **********************/
-function start(){
+
 writeline();
 OUTPUT.innerHTML+="<h2>Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars</h2>";
 OUTPUT.innerHTML+="<h2> You were born in " + birthyear+ "</h2>";
@@ -45,7 +45,7 @@ OUTPUT.innerHTML+="<h2>In 10 years you will be " + oldage + " years old</h2>";
 OUTPUT.innerHTML+="<h2>You have " +money+ " dollars</h2>";
 OUTPUT.innerHTML+="<h2>You spend half of your money, now you have" + newmoney+ "dollars</h2>";
 OUTPUT.innerHTML+="<h2>Then you get $3, now you have " +newermoney+ " dollars</h2>";
-}
+
  /*****************************
  functions
  ***************************/
