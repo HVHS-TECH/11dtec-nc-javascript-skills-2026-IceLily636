24b719e4734f296d3032128b726b04f3f199d50c
IceLily636 <24196ks@hvhs.school.nz>
2026-05-11 23:47:29 +0000

    May 11, 2026, 11:47 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,2 +1,57 @@
-/* Paste your code from the last task */
-/* Paste your code from the last task */
+
+/****************************
+Name of task: conditionals
+****************************/
+console.log("t02_variables.js")
+console.log("hi")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+// variables
+
+let username;
+let year = 2026;
+let age;
+let money;
+let birthyear =2011;
+let oldage;
+let newmoney;
+let newermoney;
+
+function writeline(){
+    OUTPUT.innerHTML = "<h1>welcome to the shop</h1>";
+}
+
+function getFormInput(){
+let NAME_FIELD = document.getElementById("nameField");
+username = NAME_FIELD.value;
+let AGE_FIELD = document.getElementById("ageField");
+age = Number(AGE_FIELD.value);
+let MONEY_FIELD = document.getElementById("moneyField");
+money = Number(MONEY_FIELD.value);
+newmoney = money/2;
+oldage = age + 10;
+newermoney = newmoney+3;
+console.log("Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars");
+console.log("You were born in " + birthyear)
+console.log("In 10 years you will be " + oldage + " years old")
+console.log("You have " +money+ " dollars")
+console.log("You spend half of your money, now you have" + newmoney+ "dollars")
+console.log("Then you get $3, now you have " +newermoney+ " dollars")
+
+/****************************
+ main code
+ **********************/
+
+writeline();
+OUTPUT.innerHTML += "<p>Your name is"+username+"</p>";
+OUTPUT.innerHTML+="<h2>Hi " + username +" As of " + year + " you are " + age + " years old. As of " + year + " you have " + money + "dollars</h2>";
+OUTPUT.innerHTML+="<h2> You were born in " + birthyear+ "</h2>";
+OUTPUT.innerHTML+="<h2>In 10 years you will be " + oldage + " years old</h2>";
+OUTPUT.innerHTML+="<h2>You have " +money+ " dollars</h2>";
+OUTPUT.innerHTML+="<h2>You spend half of your money, now you have" + newmoney+ "dollars</h2>";
+OUTPUT.innerHTML+="<h2>Then you get $3, now you have " +newermoney+ " dollars</h2>";
+
+ /*****************************
+ functions
+ ***************************/
+}
+
