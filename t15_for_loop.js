a0fcc1a5bc81d8dbead40cc79bcfb2e2262aecee
IceLily636 <24196ks@hvhs.school.nz>
2026-05-14 21:51:59 +0000

    May 14, 2026, 9:51 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -78,8 +78,8 @@ if(newermoney >= 4){
 else{
     OUTPUT.innerHTML +="<p>A chocolate bar is 4 dollars ,you cant afford it.</p>"
 }
-for(let i = verse; i> 0; i--)
+for(let i = verse; i> 0; i--){
 OUTPUT.innerHTML += "<p>" +i+ "bottles of coffee on the wall</p>";
-
+}
 }
 
